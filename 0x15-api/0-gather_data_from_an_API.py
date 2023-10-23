c8007aa496b88f1ac5dbfc7e8615fbedb82556d0
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-10-23 16:51:59 -0700

    fix first line

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
         if (el.get("completed") is True):
             todoDone.append("\t " + el.get("title"))
             done += 1
-    print('Employee {} is done with tasks({}/{})\n{}'.format(
+    print('Employee {} is done with tasks({}/{}):\n{}'.format(
         userF["name"], done, len(todosF),  "\n".join(todoDone)))
