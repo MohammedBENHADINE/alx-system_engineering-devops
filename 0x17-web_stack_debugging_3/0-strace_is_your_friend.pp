b0817a5f7613fe858e895c654273885f985c3417
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2023-11-08 23:09:58 +0100

    fix puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix typo
 exec { 'fix_typo':
-	command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	provider => 'shell'
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  provider => 'shell'
 }
